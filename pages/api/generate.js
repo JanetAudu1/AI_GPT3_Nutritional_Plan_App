e064ef35443a1dcd7ee7b0d136b90514ff67c576
onyecheaudu <janet.audu13@gmail.com>
2023-01-30 19:39:13 -0500

    test

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,27 +8,17 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = 
 `
-Create a weekly nutritional plan for the given medical conditions for a special cuisine, along with the nutritional benefits of each food.
-\n`
-const userInput1 = "Condition: "
-//const userInput2 = "Cuisine: "
+Write me a twitter thread in the style of Donald Trump with the title below. Make it sound somewhat obnoxious like he does. Use hashtags. Make it sound like he's tweeting from his phone.
 
+Title:
+`
 const generateAction = async (req, res) => {
-    const userInput3 = `${userInput1}${req.body.userInputCondition}\n`;
-    console.log(`user Input: ${userInput3}`);
-
-//    const userInput4 = `${userInput2}${req.body.userInputCuisine}\n`;
-//    console.log(`user Input: ${userInput4}`);
-
-
     // Run first prompt
-    console.log(`API: ${basePromptPrefix}${userInput3}`);
-//    console.log(`API: ${basePromptPrefix}${userInput3}${userInput4}`);
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
     const baseCompletion = await openai.createCompletion({
-	model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${userInput3}\n`,
-//        prompt: `${basePromptPrefix}${userInput3}${userInput4}\n`,
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}\n`,
         temperature: 0.8,
         max_tokens: 250,
     });
